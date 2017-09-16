fb2f9b421cba3d360ed296318197549c2cd50e6b
adeelibr <adeelimranr@gmail.com>
2017-09-17 01:12:34 +0500

    server configured with express

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors')
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    return res.status(200).json({ success: true }).end();
+})
+
+app.listen(process.env.PORT || 8081);
