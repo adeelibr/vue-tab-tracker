4b2e849b4ac6c74b25c36e14f27451fe6774c9ff
adeelibr <adeelimranr@gmail.com>
2017-09-17 16:37:09 +0500

    Setting up register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,43 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="Enter your email"
+    />
+    <input 
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="Enter your password"
+    />
+    <button 
+      @click="register"
+    >
+      Register
+    </button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  name: 'hello',
   data () {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log('register button was clicked', response.data)
+    }
   }
 }
 </script>
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export default () => {
-	return axios.create({
-			baseUrl: 'http://localhost:8081/'
-	});
+  return axios.create({ baseURL: 'http://localhost:8081/' })
 }
